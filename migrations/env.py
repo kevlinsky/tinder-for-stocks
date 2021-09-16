96a44137f0f8982e974ff0cc96950c9defd4b409
Artyom Elin <kevlinsky@outlook.com>
2021-09-16 15:09:29 +0300

    Some changes

--- migrations/env.py
+++ migrations/env.py
@@ -7,8 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 
-import models
-import db
+from app import db, models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
